9f575137c8807be3f1ebf8d562de95d6721ce084
imhwarang <imhwarang4@naver.com>
2026-01-18 23:31:56 +0900

    프로그래머스_멀리뛰기

---
+++ 프로그래머스/Level2/멀리뛰기.py
@@ -0,0 +1,11 @@
+def solution(n):
+    
+    dp = [0] * 2001
+    
+    dp[1] = 1
+    dp[2] = 2
+    
+    for i in range(3, 2001):
+        dp[i] = dp[i - 2] + dp[i - 1]
+    
+    return dp[n] % 1234567
